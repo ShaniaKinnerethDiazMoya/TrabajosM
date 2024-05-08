88ab1e6f3e71e43e091801a48734db20dc03ee46
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-05-07 23:54:43 -0600

    Add files via upload

---
+++ operacionesAritmeticas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    printf("No.1 %d \n", 5);
+    printf("No.2 %d \n", 8);
+    printf("La suma de %d + %d es = %d\n", 5,8,5+8);
+    printf("No.1 %d \n", 78787);
+    printf("No.2 %d \n", 3259);
+    printf("La suma de %d + %d es = %d\n",78787,3259,78787+3259);
+    system("Pause");
+    return 0;
+}
